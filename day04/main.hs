dcad8c0b06d8b9c434d0cb017ce2b55f9a45ee96
Jim Witschey <jim.witschey@gmail.com>
2023-12-04 22:24:36 -0500

    Day 4 part 2 solution

--- day04/main.hs
+++ day04/main.hs
@@ -6,7 +6,8 @@
 -}
 
 import           Data.Char  (isDigit)
-import           Data.List  (stripPrefix)
+import           Data.List  (sort, stripPrefix)
+import qualified Data.Map   as Map
 import           Data.Maybe (fromMaybe)
 import qualified Data.Set   as Set
 
@@ -25,12 +26,53 @@ score card = case matchCount card of
 
 data Card = Card { winners:: [String] , numbers :: [String] } deriving (Show)
 
+data IntMultiset = IntMultiset (Map.Map Int Int) deriving Show
+insertMany :: Int -> IntMultiset -> Int -> IntMultiset
+insertMany addend (IntMultiset m) x = IntMultiset $ Map.alter inc x m
+    where
+        inc Nothing  = Just addend
+        inc (Just n) = Just $ n + addend
+
+insert ims x = insertMany 1 ims x
+
+insertAll :: IntMultiset -> [Int] -> IntMultiset
+insertAll ims = foldl insert ims
+
+lookupIMS :: Int -> IntMultiset -> Int
+lookupIMS k (IntMultiset m) = Map.findWithDefault 1 k m
+
+unwrap (IntMultiset m) = m
+
+copiesOfCards :: Map.Map Int Int -> IntMultiset
+copiesOfCards matchCount = foldl (copiesOfCards' matchCount) init remainingKeys
+    where
+        init = (IntMultiset Map.empty) `insertAll` (Map.keys matchCount)
+        remainingKeys = Map.keys matchCount  -- depends on Map.keys returning results sorted
+
+copiesOfCards' :: Map.Map Int Int -> IntMultiset -> Int -> IntMultiset
+copiesOfCards' matchCount acc i = foldl (insertMany cardsAtIdx) acc [i+1..i+matches]
+    where
+        cardsAtIdx = lookupIMS i acc
+        matches = Map.findWithDefault 0 i matchCount
+
+
 main = do
     -- part 1
     s <- readFile "example"
-    let cardsP1 = map parseLine $ lines s
-    putStr "scores: "
-    print $ sum $ map score cardsP1
+    let cardsEx = map parseLine $ lines s
+    print $ sum $ map score cardsEx
 
     i <- readFile "input"
     print $ sum $ map (score . parseLine) $ lines i
+
+    -- part 2
+    -- example
+    let matchCounts = Map.fromList $ zip [1..] $ map matchCount cardsEx
+    print $ (unwrap . copiesOfCards) matchCounts
+    print $ (sum . Map.elems . unwrap . copiesOfCards) matchCounts
+
+    -- input
+    let cardsInput = map parseLine $ lines i
+    let matchCountsInput =  Map.fromList $ [1..] `zip` map matchCount cardsInput
+    print $ (sum . Map.elems . unwrap . copiesOfCards) matchCountsInput
+
