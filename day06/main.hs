74e897a5a3e8817c6400521fd5576ea2ce81e97f
Jim Witschey <jim.witschey@gmail.com>
2023-12-06 20:19:11 -0500

    Day 6 part 2

--- day06/main.hs
+++ day06/main.hs
@@ -13,13 +13,19 @@ readMaybeInt :: String -> Maybe Int
 readMaybeInt = readMaybe
 
 data Race = Race { meters :: Int, recordMs :: Int } deriving Show
-parseLine = catMaybes . map readMaybeInt . words
-parse s = map (uncurry Race) $ zip line1 line2
-  where line1: line2: _ = map parseLine . lines $ s
 
+-- part 1
+parseLine1 = catMaybes . map readMaybeInt . words
+parse1 s = map (uncurry Race) $ zip line1 line2
+  where line1: line2: _ = map parseLine1 . lines $ s
 numberOfRecordBreakers (Race meters record) = length $ filter (>record) $ possibleDistances meters
 possibleDistances meters = [speed * (meters - speed) | speed <- [0..meters]]
 
+-- part 2
+parse s = Race (read metersStr) (read recordStr)
+  where metersStr : recordStr : _ = map (filter isDigit) $ lines s
+
 main = do
     s <- readFile "input"
-    print $ product $ map numberOfRecordBreakers $ parse s
+    print $ product $ map numberOfRecordBreakers $ parse1 s
+    print $ numberOfRecordBreakers $ parse s
